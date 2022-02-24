1c44a7cb0a32c0951088734d220a91a73eb4d458
francis <tan.huynh@setel.com>
2022-02-25 00:55:04 +0700

    feat: update stack

--- stack/script.py
+++ stack/script.py
@@ -1,14 +1,49 @@
+class _Node:
+    def __init__(self, value, next_node=None):
+        self.__value = value
+        self.__next_node = next_node
+
+    def get_value(self):
+        return self.__value
+
+    def get_next_node(self):
+        return self.__next_node
+
+    def set_next_node(self, next_node):
+        self.__next_node = next_node
+
+
 class Stack:
-    def __init__(self):
-        self.__stack = []
-
-    def append(self, value: any):
-        self.__stack.append(value)
-
-    def pop(self):
-        self.__stack = self.__stack[:-1]
+    def __init__(self, limit=1000):
+        self.__top_item = None
+        self.__limit = limit
+        self.__size = 0
 
     def peek(self):
-        return self.__stack[-1] if len(self.__stack) else None
+        if not self.is_empty():
+            return self.__top_item.get_value()
+        print('The stack is empty!')
 
+    def push(self, value):
+        if self.has_space():
+            item = _Node(value)
+            item.set_next_node(self.__top_item)
+            self.__top_item = item
+            self.__size += 1
+        else:
+            print('All out of space!')
+
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.__top_item
+            self.__top_item = item_to_remove.get_next_node()
+            self.__size -= 1
+            return item_to_remove.get_value()
+        print('The stack is empty!')
+
+    def has_space(self):
+        return self.__size < self.__limit
+
+    def is_empty(self):
+        return self.__size == 0
 
--- stack/test.py
+++ stack/test.py
@@ -1,21 +1,63 @@
-import unittest
-
+import pytest
+import io
+import sys
 from script import Stack
 
 
-class StackTestCase(unittest.TestCase):
-    def test_stack(self):
-        my_stack = Stack()
-        self.assertIsNone(my_stack.peek())
-        my_stack.append(1)
-        self.assertEqual(my_stack.peek(), 1)
-        my_stack.append(2)
-        self.assertEqual(my_stack.peek(), 2)
-        my_stack.pop()
-        self.assertEqual(my_stack.peek(), 1)
-        my_stack.pop()
-        self.assertIsNone(my_stack.peek())
+def test_create_stack():
+    s = Stack()
+    assert s is not None
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture()
+def empty_s():
+    return Stack()
+
+
+@pytest.fixture()
+def limit_s():
+   return Stack(1)
+
+
+@pytest.fixture()
+def full_s():
+    s = Stack(1)
+    s.push(1)
+    return s
+
+
+def test_peek(empty_s, full_s):
+    assert full_s.peek() == 1
+
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    empty_s.peek()
+    assert captured_output.getvalue().strip() == 'The stack is empty!'
+    sys.stdout = sys.__stdout__
+
+
+def test_push(empty_s, limit_s, full_s):
+    assert empty_s.is_empty() is True
+    empty_s.push(1)
+    assert empty_s.is_empty() is False
+
+    assert limit_s.has_space() is True
+    limit_s.push(1)
+    assert limit_s.has_space() is False
+
+    assert full_s.has_space() is False
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    full_s.push(1)
+    assert captured_output.getvalue().strip() == 'All out of space!'
+    sys.stdout = sys.__stdout__
+
+
+def test_pop(full_s):
+    assert full_s.peek() == 1
+    assert full_s.pop() == 1
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    full_s.pop()
+    assert captured_output.getvalue().strip() == 'The stack is empty!'
+    sys.stdout = sys.__stdout__
